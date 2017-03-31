c5ad8a0f5cb7f548dd40a3113196c7d7794f7ac5
Alex <sachovskiy@list.ru>
2017-03-31 12:29:31 +0200

    Erstes ViewModel hinzugefügt

---
+++ Kalorienrechner/ViewModel/Food/IngredientViewModel.cs
@@ -0,0 +1,115 @@
+﻿using CaloryLibrary.Models;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalorienrechner.ViewModel.Food
+{
+    public class IngredientViewModel : BindableBase
+    {
+        private int _ID;
+        private string _name;
+        private double _calories;
+        private double _protein;
+        private double _fat;
+        private double _carbs;
+        private ObservableCollection<Unit> _units;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                SetProperty(ref _name, value);
+            }
+        }
+
+        public double Calories
+        {
+            get
+            {
+                return _calories;
+            }
+
+            set
+            {
+                SetProperty(ref _calories, value);
+
+            }
+        }
+
+        public double Protein
+        {
+            get
+            {
+                return _protein;
+            }
+
+            set
+            {
+                SetProperty(ref _protein, value);
+            }
+        }
+
+        public double Fat
+        {
+            get
+            {
+                return _fat;
+            }
+
+            set
+            {
+                SetProperty(ref _fat, value);
+            }
+        }
+
+        public double Carbs
+        {
+            get
+            {
+                return _carbs;
+            }
+
+            set
+            {
+                SetProperty(ref _carbs, value);
+            }
+        }
+
+        public ObservableCollection<Unit> Units
+        {
+            get
+            {
+                return _units;
+            }
+
+        private    set
+            {
+                _units = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _ID;
+            }
+
+          private  set
+            {
+                _ID = value;
+                SetProperty(ref _ID, value);
+            }
+        }
+    }
+}
