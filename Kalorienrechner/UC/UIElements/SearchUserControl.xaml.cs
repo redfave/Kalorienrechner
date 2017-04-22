9bd3e1207d990ed4088740cc4ae4a5c10bf08a62
redfave <sachovskiy@list.ru>
2017-04-22 19:47:17 +0200

    Continue adding content support in search UC

--- Kalorienrechner/UC/UIElements/SearchUserControl.xaml.cs
+++ Kalorienrechner/UC/UIElements/SearchUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kalorienrechner.ViewModel.UIElements;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Kalorienrechner.UC.UIElements
         public SearchUserControl()
         {
             InitializeComponent();
+            SearchViewModel<> searchViewModelInstance = new SearchViewModel<>();
+            LayoutGrid.DataContext = SearchViewModel;
         }
     }
 }
--- Kalorienrechner/ViewModel/UIElements/SearchViewModel.cs
+++ Kalorienrechner/ViewModel/UIElements/SearchViewModel.cs
@@ -1,13 +1,81 @@
-﻿using Prism.Mvvm;
+﻿using CaloryLibrary.Repository;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kalorienrechner.ViewModel.UIElements
 {
-    public class SearchViewModel : BindableBase
+    public class SearchViewModel<T> : BindableBase
     {
+        private CaloryRepository _entityContext = new CaloryRepository();
+        private bool _showOnlyFavorites;
+        private string _searchString;
+        private ObservableCollection<T> _resultCollection = new ObservableCollection<T>();
+        private Type _searchType;
+
+        public SearchViewModel()
+        {
+            //_entityContext.Get();
+        }
+        public bool ShowOnlyFavorites
+        {
+            get
+            {
+                return _showOnlyFavorites;
+            }
+
+            set
+            {
+                _showOnlyFavorites = value;
+                SetProperty(ref _showOnlyFavorites, value);
+            }
+        }
+
+        public string SearchString
+        {
+            get
+            {
+                return _searchString;
+            }
+
+            set
+            {
+                _searchString = value;
+                SetProperty(ref _searchString, value);
+
+            }
+        }
+
+        public ObservableCollection<T> ResultCollection
+        {
+            get
+            {
+                return _resultCollection;
+            }
+            set
+            {
+                _resultCollection = value;
+                SetProperty(ref _resultCollection, value);
+            }
+        }
+
+        public Type SearchType
+        {
+            get
+            {
+                return _searchType;
+            }
+
+            set
+            {
+                _searchType = value;
+                SetProperty(ref _searchType, value);
+            }
+        }
     }
+
 }
