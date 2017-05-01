8019581684c46494e8ba13dfac5988f3bccd87ff
redfave <sachovskiy@list.ru>
2017-05-01 22:03:21 +0200

    Added global available settings

---
+++ Kalorienrechner/Global.cs
@@ -0,0 +1,25 @@
+﻿using CaloryLibrary.Repository;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalorienrechner
+{
+    public static class Global
+    {
+        public static string CurrentUser
+        {
+            get;
+            set;
+        }
+
+        public static bool ConnectionEstablished
+        {
+            get;
+            set;
+        }
+    }
+}
