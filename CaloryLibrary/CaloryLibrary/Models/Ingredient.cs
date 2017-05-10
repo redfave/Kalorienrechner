fcc8ae89202d3e0f5b9b63973e9cbf1a28d70d0e
Zauberer1994 <merlin@hearts-of-psp.de>
2017-05-10 18:51:21 +0200

    Ingredient Model changed

--- CaloryLibrary/CaloryLibrary/Models/Ingredient.cs
+++ CaloryLibrary/CaloryLibrary/Models/Ingredient.cs
@@ -10,10 +10,10 @@ namespace CaloryLibrary.Models
     {
         public int IngredientId { get; set; }
         public string Name { get; set; }
-        public double Calories { private get; set; }
-        public double Protein { private get; set; }
-        public double Fat { private get; set; }
-        public double Carbs { private get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Fat { get; set; }
+        public double Carbs { get; set; }
         public Unit BaseUnit { get; set; }
     }
 }
