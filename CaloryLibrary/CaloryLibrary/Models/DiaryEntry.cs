6d91a9477d3defc7567338c1b77fbd467f46c9e1
Merlin Tsu <merlin.tsu@wsw-online.de>
2017-05-11 13:29:49 +0200

    DiaryEntry Model added

---
+++ CaloryLibrary/CaloryLibrary/Models/DiaryEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaloryLibrary.Models
+{
+    public class DiaryEntry
+    {
+        public int DiaryEntryId { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Login Creator { get; set; }
+        public virtual Ingredient Ingredient { get; set; }
+        public virtual Recipe Recipe { get; set; }
+    }
+}
