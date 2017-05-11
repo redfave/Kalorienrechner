8e25521d2ceff0d66ad99dffaa8a11d613739de9
redfave <sachovskiy@list.ru>
2017-05-12 00:15:23 +0200

    Allowing registration of new user accounts

---
+++ Kalorienrechner/Helper/Converter/PasswordBoxMultiConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Kalorienrechner.Helper.Converter
+{
+    public class PasswordBoxMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Tuple.Create(values[0] as PasswordBox, values[1] as PasswordBox);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kalorienrechner/ViewModel/User/RegisterViewModel.cs
@@ -0,0 +1,137 @@
+﻿using CaloryLibrary.Models;
+using CaloryLibrary.Repository;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kalorienrechner.ViewModel.User
+{
+    public class RegisterViewModel : BindableBase
+    {
+        private string _userName;
+        private bool _displayWrongCredentialsError;
+        private bool _displaySuccessfulRegistrationNotification;
+        private bool _isBusy;
+        private CaloryRepository repo = new CaloryRepository();
+
+
+        public RegisterViewModel()
+        {
+            RegisterCommand = new DelegateCommand<Tuple<PasswordBox, PasswordBox>>(ExecuterRegister, CanRegister);
+        }
+
+        public DelegateCommand<Tuple<PasswordBox, PasswordBox>> RegisterCommand
+        {
+            get; private set;
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                SetProperty(ref _userName, value);
+                RaiseRegisterCommandCanExecuteChanged();
+            }
+        }
+
+        public bool DisplayWrongCredentialsError
+        {
+            get
+            {
+                return _displayWrongCredentialsError;
+            }
+
+            private set
+            {
+                SetProperty(ref _displayWrongCredentialsError, value);
+            }
+        }
+
+        public bool DisplaySuccessfulRegistrationNotification
+        {
+            get
+            {
+                return _displaySuccessfulRegistrationNotification;
+            }
+
+            set
+            {
+                SetProperty(ref _displaySuccessfulRegistrationNotification, value);
+            }
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+
+            set
+            {
+                SetProperty(ref _isBusy, value);
+            }
+        }
+
+        public void RaiseRegisterCommandCanExecuteChanged()
+        {
+            //Called by the view when the password is changed so the register button can be enabled
+            RegisterCommand.RaiseCanExecuteChanged();
+        }
+
+        private async void ExecuterRegister(Tuple<PasswordBox, PasswordBox> passwordTuple)
+        {
+                        if (passwordTuple == null)
+            {
+                throw new ArgumentNullException();
+            }
+            IsBusy = true;
+            Login newUser = new Login();
+            newUser.Name = UserName;
+            newUser.Password = CaloryLibrary.Helper.CalculateMD5Hash(passwordTuple.Item1.Password);
+            await CreateNewUserAsync(newUser);
+            DisplayWrongCredentialsError = false;
+            DisplaySuccessfulRegistrationNotification = true;
+            IsBusy = false;
+        }
+
+        private bool CanRegister(Tuple<PasswordBox, PasswordBox> passwordTuple)
+        {
+            if (passwordTuple == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (UserName != null && UserName.Count() > 0 && passwordTuple.Item1.Password != null && passwordTuple.Item1.Password.Count() > 0 && passwordTuple.Item2.Password != null && passwordTuple.Item2.Password.Count() > 0)
+            {
+                if (passwordTuple.Item1.Password == passwordTuple.Item2.Password)
+                {
+                    DisplaySuccessfulRegistrationNotification = false;
+                    DisplayWrongCredentialsError = false;
+                    return true;
+                }
+                DisplaySuccessfulRegistrationNotification = false;
+                DisplayWrongCredentialsError = true;
+            }
+            return false;
+        }
+
+        private Task CreateNewUserAsync(Login login)
+        {
+            return Task.Run(() =>
+            {
+                repo.Create(login);
+                repo.Save();
+            }
+                );
+        }
+    }
+}
